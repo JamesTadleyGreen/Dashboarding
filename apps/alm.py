c235de7e88857d51a61ac5b6f0f16b41b253208d
James Green <jamestadleygreen@outlook.com>
2020-12-19 23:59:40 +0000

    Fixing empty file issue

--- apps/alm.py
+++ apps/alm.py
@@ -1,1 +1,21 @@
-layout = None
+import dash
+import dash_table as dt
+import dash_core_components as dcc
+import dash_html_components as html
+import dash_bootstrap_components as dbc
+from dash.dependencies import Input, Output
+import plotly.express as px
+import plotly.graph_objects as go
+import pandas as pd
+
+from app import app
+
+
+
+
+layout = html.Div(children=[
+    # Header
+    html.H1(children='Project King'),
+    # Subheader
+    html.H4(children='ALM.')
+])
