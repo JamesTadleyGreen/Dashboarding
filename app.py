282cb958db811648c0036c3ee08426e29f22bb7f
James Green <jamestadleygreen@outlook.com>
2020-12-20 01:38:06 +0000

    Adding dbc.LUX theme to the stylesheets

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import dash
 import dash_bootstrap_components as dbc
 
 # CSS STYLING -------------------------------------------------------
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css', dbc.themes.LUX]
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 server = app.server
 
